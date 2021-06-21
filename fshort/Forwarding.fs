07e6a22b7bc56c989c925bc5baaf765cbae6a931
Philipp Scheede <p.scheede@gmx.de>
2021-06-21 22:55:24 +0200

    Make getting of shortened url more modular

--- fshort/Forwarding.fs
+++ fshort/Forwarding.fs
@@ -3,13 +3,18 @@ module fshort.Forwarding
 open Giraffe
 open Microsoft.AspNetCore.Http
 
-let links =
+type Urls = Map<string, string>
+
+let links: Urls =
     Map.empty
     |> Map.add "gog" "https://google.com"
     |> Map.add "ytbe" "https://youtube.com"
+    
+let getLongUrl shortUrl =
+    Map.tryFind shortUrl links
 
-let forward (subRoute: string) : HttpHandler =
+let forward (shortUrl: string) : HttpHandler =
     fun (next : HttpFunc) (ctx : HttpContext) ->
-        match Map.tryFind subRoute links with
+        match getLongUrl shortUrl with
         | Some url -> redirectTo false url next ctx
         | None -> RequestErrors.NOT_FOUND "Not Found" next ctx
